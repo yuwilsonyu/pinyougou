77e4848eea82c37c361bd9cf2ae1c4a218177a0c
ywj <13926025708@139.com>
2018-10-30 17:09:09 +0800

    day01

---
+++ pinyougou-pojo/src/main/java/com/pinyougou/pojo/Brand.java
@@ -0,0 +1,31 @@
+package com.pinyougou.pojo;
+
+public class Brand {
+    private int id;
+    private String name;
+    private String firstChar;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstChar() {
+        return firstChar;
+    }
+
+    public void setFirstChar(String firstChar) {
+        this.firstChar = firstChar;
+    }
+}
